d845a5cd3df94c9e47f4f26e5a83538f68425eb2
mhdhassimcr717 <mhdhassimcr717@gmail.com>
2022-04-28 00:37:16 +0530

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ChatProvider from "./Context/chatProvider";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ChakraProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </ChakraProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
